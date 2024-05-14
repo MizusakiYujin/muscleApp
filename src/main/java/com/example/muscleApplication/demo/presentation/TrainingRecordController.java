b34ae12afffe1d65fd1a8f49f833ea9f1b2049a1
MizusakiYujin <mizusaki_yujin@ims-sol.co.jp>
2024-05-14 23:52:35 +0900

    登録した種目名のリストを返すように修正

--- src/main/java/com/example/muscleApplication/demo/presentation/TrainingRecordController.java
+++ src/main/java/com/example/muscleApplication/demo/presentation/TrainingRecordController.java
@@ -1,6 +1,7 @@
 package com.example.muscleApplication.demo.presentation;
 
 import com.example.muscleApplication.demo.application.TrainingRecordService;
+import com.example.muscleApplication.demo.domain.Part;
 import com.example.muscleApplication.demo.domain.TrainingRecord;
 import com.example.muscleApplication.demo.presentation.response.TrainingRecordResponseList;
 import lombok.RequiredArgsConstructor;
@@ -26,14 +27,20 @@ public class TrainingRecordController {
     }
 
     @PostMapping
-    public ResponseEntity<List<TrainingRecord>> postTrainingRecordList(
+    public ResponseEntity<List<String>> postTrainingRecordList(
             @RequestBody
             List<TrainingRecord> trainingRecordList
     ) {
         Integer result = trainingRecordService.execute(trainingRecordList);
         if (result.equals(trainingRecordList.size())) {
+            List<String> trainingNameList = trainingRecordList
+                    .stream()
+                    .map(TrainingRecord::part)
+                    .map(Part::trainingName)
+                    .distinct()
+                    .toList();
             return new ResponseEntity<>(
-                    trainingRecordList,
+                    trainingNameList,
                     null,
                     HttpStatus.OK
             );
@@ -41,7 +48,7 @@ public class TrainingRecordController {
             return new ResponseEntity<>(
                     null,
                     null,
-                    HttpStatus.CONFLICT
+                    HttpStatus.NO_CONTENT
             );
         }
     }
